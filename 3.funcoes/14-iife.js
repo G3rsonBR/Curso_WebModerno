cfb9506602886be64b5082ac04dcbfa7114e2322
Gerson Silva <107651514+gersonsbr@users.noreply.github.com>
2023-03-01 09:14:42 -0300

    IIFE

---
+++ 3.funcoes/14-iife.js
@@ -0,0 +1,10 @@
+// IIFE -> Immediately Invoked Function Expression
+
+(function(){
+    console.log('Será Executado na Hora')
+    console.log('Foge do Escopo mais Abrangente')
+})()
+
+//Semelhante: 
+console.log('Será Executado na Hora')
+console.log('Foge do Escopo mais Abrangente')

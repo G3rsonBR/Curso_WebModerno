550e8e85784c02322346c8e680c85df362bb5ee7
G3rsonBR <gersons.br13@gmail.com>
2023-03-13 16:41:29 -0300

    Pasta Objetos e Arquivo OO

---
+++ 4.objetos/#1-oo.js
@@ -0,0 +1,24 @@
+// Orientação a Objeto 
+// Código não executável
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// O.O.
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento()
+// Encapsula comportamentos e atributos
+
+// Principais Informações
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herança
+// 4. Polimorfismo
